464e6f2a283ee84c3bc5c916ffd24f1acdbe3d7e
henglovejia <932805400@qq.com>
2018-09-28 18:21:55 +0800

    代码高亮

--- app/src/main/java/test/bank/bettercoder/questions/api/QuestionApi.java
+++ app/src/main/java/test/bank/bettercoder/questions/api/QuestionApi.java
@@ -6,13 +6,21 @@ import test.bank.bettercoder.questions.model.QuestionBean;
 import test.bank.bettercoder.questions.model.QuestionModel;
 import test.bank.bettercoder.questions.model.SubjectBean;
 import test.bank.bettercoder.questions.model.SubjectModel;
+import test.bank.bettercoder.questions.model.TreeBean;
+import test.bank.bettercoder.questions.model.TreeModel;
 import test.bank.bettercoder.utils.request.SimpleCall;
 
 public interface QuestionApi {
+    //获取可用学科
     @POST("BetterCoder/controller/QuestionService/initSubject")
     SimpleCall<SubjectModel> getSubject(@Body SubjectBean bean);
+    //继续上次做题
     @POST("BetterCoder/controller/QuestionService/continueExercise")
     SimpleCall<QuestionModel> continueQuestions(@Body QuestionBean bean);
+    //重新开始做题
     @POST("BetterCoder/controller/QuestionService/startExercise")
     SimpleCall<QuestionModel> startQuestions(@Body QuestionBean bean);
+    //获取知识树
+    @POST("BetterCoder/controller/QuestionService/initTree")
+    SimpleCall<TreeModel> getTrees(@Body TreeBean bean);
 }
